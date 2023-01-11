53d71fdf39772850b9793f880e77acecb75ad6f1
Istiak Hossain <istiakhossainofficial@gmail.com>
2023-01-11 16:18:10 +0600

    part5

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -25,7 +25,8 @@ class DatabaseSeeder extends Seeder
 
         $this->create_user_with_role('Super Admin', 'Super Admin', 'super-admin@lms.com' );
         $this->create_user_with_role('Communication', 'Communication Team', 'communication@lms.com' );
-       $teacher = $this->create_user_with_role('Teacher', 'Teacher', 'teacher@lms.com' );
+        $teacher = $this->create_user_with_role('Teacher', 'Teacher', 'teacher@lms.com' );
+        $this->create_user_with_role('Leads', 'Leads', 'lead@lms.com' );
 
 
         // Create Leads
